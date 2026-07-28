4ba79994680738638b007f0e8b5266cb6fe246ea
aoliveroc <aoliveroc@unal.edu.co>
2026-07-28 18:31:33 -0500

    Actividad 6 POO

---
+++ ejercicio 4.6.py
@@ -0,0 +1,27 @@
+class Profesor:
+
+    def imprimir(self):
+        print("Es un profesor.")
+
+
+class ProfesorTitular(Profesor):
+
+    def __init__(self):
+        self.años = 0
+
+    def imprimir(self):
+        print("Es un profesor titular.")
+
+    def imprimir_años(self):
+        print("Años =", self.años)
+
+
+def main():
+
+    profesor1 = ProfesorTitular()
+
+    profesor1.imprimir_años()
+
+
+if __name__ == "__main__":
+    main()
